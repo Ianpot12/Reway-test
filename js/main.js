71ddcc0a23f50356c6f8f1b71232d0680014afd2
ian potgieter <ian@webstreet.co.za>
2020-02-20 08:29:10 +0200

    final styling

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
 
-    
+    /**
+     * offcanvas sidebar
+     */
+
     $('.sidebar_offcanvas_trigger').click(function (e) {
         e.preventDefault();
         $(this).parents('.sidebar_offcanvas_container').toggleClass('open');
@@ -10,6 +13,34 @@ $(document).ready(function() {
         e.preventDefault();
         $(this).parents('.sidebar_offcanvas_container').toggleClass('open');
     });
- 
+
+    /**
+     * add class
+     */
+    if ($('.open').length > 0) {
+        $('.sidebar_offcanvas').removeClass('close');
+    }
+
+    $('.sidebar_offcanvas_trigger').on('click', function(e){
+        $('.sidebar_offcanvas').removeClass('close');
+      });
+    $('.sidebar_offcanvas_trigger_close').on('click', function(e){
+        e.preventDefault();
+        $('.sidebar_offcanvas').addClass('close');
+      });
+
+
+    $('.btn-close').on('click', function (e) {
+        $('.row-subscribe').addClass('d-none');
+    });
+
+
+      /**
+       * rangeslider
+       */
+    
+  
 
 });
+
+
